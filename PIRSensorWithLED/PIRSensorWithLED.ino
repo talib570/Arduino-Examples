679eda7f887c9f085884b5399a8dab6ee414ad63
Talib Allauddin <taliballauddin@gmail.com>
2020-11-15 03:11:21 +0500

    Added formatting

--- PIRSensorWithLED/PIRSensorWithLED.ino
+++ PIRSensorWithLED/PIRSensorWithLED.ino
@@ -3,26 +3,26 @@ int sensor = 5; // Sensor oin
 int state = LOW;
 int val = 0;
 void setup() {
-pinMode(Buzzer, OUTPUT);
-pinMode(sensor, INPUT);
-Serial.begin(9600);
+  pinMode(Buzzer, OUTPUT);
+  pinMode(sensor, INPUT);
+  Serial.begin(9600);
 }
 void loop(){
-val = digitalRead(sensor);
-if (val == HIGH) {
-digitalWrite(Buzzer, HIGH);
-delay(100);
-if (state == LOW) {
-Serial.println("ACTION REQUARED MOTION DETECTED");
-state = HIGH;
-}
-}
-else {
-digitalWrite(Buzzer, LOW);
-delay(20);
-if (state == HIGH){
-Serial.println("NO ACTION REQUARED");
-state = LOW;
-}
-}
+  val = digitalRead(sensor);
+  if (val == HIGH) {
+    digitalWrite(Buzzer, HIGH);
+    delay(100);
+    if (state == LOW) {
+      Serial.println("ACTION REQUARED MOTION DETECTED");
+      state = HIGH;
+    }
+  }
+  else {
+    digitalWrite(Buzzer, LOW);
+    delay(20);
+    if (state == HIGH){
+      Serial.println("NO ACTION REQUARED");
+      state = LOW;
+    }
+  }
 }
